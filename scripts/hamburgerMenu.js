bb9fcde853aeaf8cba5360ec276abb13223436f6
derrick <derrickethornton@protonmail.com>
2021-11-08 02:42:02 -0600

    Show the menu when the hamburger menu is pressed.

--- scripts/hamburgerMenu.js
+++ scripts/hamburgerMenu.js
@@ -1,14 +1,15 @@
 const menu = document.querySelector('.menu-button');
+const sidebar = document.querySelector('#sidebar');
 let menuOpen = false;
 menu.addEventListener('click', () => {
     if(!menuOpen) {
         menu.classList.add('open');
         menuOpen = true;
-        console.log('works');
+        sidebar.style = 'display: block;';
     }
     else {
         menu.classList.remove('open');
+        sidebar.style = 'display: none;'
         menuOpen = false;
     }
 })
-console.log('works');
